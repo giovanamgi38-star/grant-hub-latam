1f575f7befb388d764fbae4c5a9e34bd1a06b517
giovanamgi38-star <giovanamgi38@gmail.com>
2026-05-22 16:33:34 -0600

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -2,16 +2,7 @@ import { createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
 export function getRouter() {
-  const router = createRouter({
+  return createRouter({
     routeTree,
-    defaultPreload: 'intent',
-    scrollRestoration: true,
-    defaultPreloadStaleTime: 0,
-    defaultErrorComponent: ({ error }) => (
-      <p>{error?.message || 'Error'}</p>
-    ),
-    defaultNotFoundComponent: () => <p>not found</p>,
   })
-
-  return router
 }
